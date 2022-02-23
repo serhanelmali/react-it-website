06d1e555c443f049b565c942c4075e7903160ab3
Serhan Elmalı <serhanelmali@gmail.com>
2022-02-23 22:36:27 +0300

    adds muted and loop props to HeroVideo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,7 +11,7 @@ import {
 const Hero = () => {
   return (
     <HeroSection>
-      <HeroVideo src="./assets/hero.mp4" autoPlay />
+      <HeroVideo src="./assets/hero.mp4" autoPlay muted loop />
       <Container>
         <MainHeading>Your data is secure with us</MainHeading>
 
