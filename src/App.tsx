2948884a7ab6bffbec2ecd258a08c2e6c3b3f2e3
Serhan Elmalı <serhanelmali@gmail.com>
2022-02-23 22:00:52 +0300

    adds pages and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import GlobalStyle from "./globalStyles"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+//Pages
+import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import Pricing from "./pages/Pricing"
+
 function App() {
-  return <div className="App">Test</div>
+  return (
+    <Router>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
